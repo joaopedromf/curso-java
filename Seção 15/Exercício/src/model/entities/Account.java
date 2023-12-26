a2a48ddbd7161a31baa4f2fa29779234b0606409
João Pedro Martins Forte <jpforte2004@gmail.com>
2023-12-25 23:50:58 -0300

    Exercícios da seção 15

---
+++ Seção 15/Exercício/src/model/entities/Account.java
@@ -0,0 +1,68 @@
+package model.entities;
+
+import model.exceptions.BusinessException;
+
+public class Account {
+    
+    private Integer number;
+    private String holder;
+    private Double balance = 0.0;
+    private Double withdrawLimit;
+
+    public Account(){
+    }
+
+    public Account(Integer number, String holder, Double initialBalance, Double withdrawLimit){
+        this.number = number;
+        this.holder = holder;
+        deposit(initialBalance);
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public Integer getNumber(){
+        return number;
+    }
+
+    public void setNumber(Integer number){
+        this.number = number;
+    }
+
+    public String getHolder(){
+        return holder;
+    }
+
+    public void setHolder(String holder){
+        this.holder = holder;
+    }
+
+    public Double getBalance(){
+        return balance;
+    }
+
+    public Double getWithdrawLimit(){
+        return withdrawLimit;
+    }
+
+    public void setWithdrawLimit(Double withdrawLimit){
+        this.withdrawLimit = withdrawLimit;
+    }
+    
+    public void deposit(Double amount){
+        balance += amount;
+    }
+
+    public void withdraw(Double amount){
+        validateWithdraw(amount);
+        balance -= amount;
+    }
+
+    private void validateWithdraw(double amount){
+        if(amount > withdrawLimit){
+            throw new BusinessException("The amount exceeds withdraw limit");
+        }
+        
+        if(amount > balance){
+            throw new BusinessException("Not enough balance");
+        }
+    }
+}
